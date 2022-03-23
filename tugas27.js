5d1f0e6dcab22818e21a2fa3ae7a35fba00ca2ea
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-03-23 17:42:49 +0800

    Tugas 27 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas27.js
@@ -0,0 +1,20 @@
+function tugas27() {
+  var kata1 = "No";
+  var kata2 = " rumah saya ";
+  var kata3 = 30;
+  var kata4 = "A";
+
+  let konversi = String(kata3);
+
+  let menyatukan = kata1.concat(kata2 + konversi + kata4);
+  console.log(menyatukan);
+
+  var kata5 = "Jalan pangeran Tirtayasa No ";
+  var kata6 = 70;
+
+  let konversi2 = String(kata6);
+
+  let menyatukan2 = kata5.concat(konversi2);
+  console.log(menyatukan2);
+}
+tugas27();
